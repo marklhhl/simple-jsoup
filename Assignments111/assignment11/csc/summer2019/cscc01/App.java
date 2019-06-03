538ec1981266e03a9a808a78de69826cf2ec18de
marklhhl <marklhhl1@gmail.com>
2019-06-03 19:22:02 -0400

    another change for dev

--- Assignments111/assignment11/csc/summer2019/cscc01/App.java
+++ Assignments111/assignment11/csc/summer2019/cscc01/App.java
@@ -10,6 +10,7 @@ public class App
     public static void main( String[] args )
     {
       // testing changes
+      // another change
         Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
     }
 }
